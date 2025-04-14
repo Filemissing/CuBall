226623245816db1392674bd6176807cd723d65f9
File_Missing <michaverheij@gmail.com>
2025-04-14 12:58:56 +0200

    added door

---
+++ Assets/Door.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(BoxCollider))]
+public class Door : MonoBehaviour
+{
+    MeshRenderer renderer;
+    BoxCollider boxCollider;
+    private void Awake()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    public void Activate()
+    {
+        renderer.enabled = false;
+        boxCollider.enabled = false;
+    }
+    public void DeActivate()
+    {
+        renderer.enabled = true;
+        boxCollider.enabled = true;
+    }
+}
